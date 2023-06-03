1a0f9d74435a45a7b63e9de2707a1895ccecb098
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2023-06-03 20:48:21 +0100

    Add the Image, Text, Row, and Column description quadrants

--- src/main/java/com/blogspot/thengnet/composablequadrant/MainActivity.kt
+++ src/main/java/com/blogspot/thengnet/composablequadrant/MainActivity.kt
@@ -3,13 +3,23 @@ package com.blogspot.thengnet.composablequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.blogspot.thengnet.composablequadrant.ui.theme.ComposableQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +40,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(modifier = modifier.weight(weight = 1F, fill = true)) {
+            Column(
+                modifier = modifier
+                    .background(color = Color(0xFFEADDFF))
+                    .padding(16.dp).weight(weight = 1F, fill = true),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.text_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(bottom = 16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.text_composable_description),
+                )
+            }
+            Column(
+                modifier = modifier
+                    .background(color = Color(0xFFD0BCFF))
+                    .padding(16.dp).weight(weight = 1F, fill = true),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.image_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(bottom = 16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.image_composable_description),
+                )
+            }
+        }
+        Row(modifier = modifier.weight(weight = 1F, fill = true)) {
+            Column(
+                modifier = modifier
+                    .background(Color(0xFFB69DF8))
+                    .padding(16.dp)
+                    .weight(weight = 1F, fill = true),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.row_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(bottom = 16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.row_composable_description),
+                )
+            }
+            Column(
+                modifier = modifier
+                    .background(color = Color(0xFFF6EDFF))
+                    .padding(16.dp)
+                    .weight(weight = 1F, fill = true),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.column_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(bottom = 16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.column_composable_description),
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposableQuadrantTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
